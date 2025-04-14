5f1cbfb00b32a2895179ad500f439dd833299478
kaoutar sissah <ffg77590@email.com>
2025-04-14 14:16:03 +0100

    SCRUM-1 add auth and update routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 
-<<<<<<< HEAD
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -8,7 +7,7 @@ export const login = async (req, res) => {
     if (email === "admin@test.com" && password === "123456") {
       const token = jwt.sign(
         { id: "1", email },
-        process.env.JWT_SECRET,
+        process.env.JWT_SECRET || "secretkey",
         { expiresIn: "1d" }
       );
 
@@ -27,22 +26,4 @@ export const login = async (req, res) => {
       error: error.message,
     });
   }
-=======
-export const login = (req, res) => {
-  const { email, password } = req.body;
-
-  if (email === "admin@test.com" && password === "123456") {
-    const token = jwt.sign(
-      { email },
-      "secretkey",
-      { expiresIn: "1d" }
-    );
-
-    return res.status(200).json({ token });
-  }
-
-  return res.status(401).json({
-    message: "Invalid credentials"
-  });
->>>>>>> 4e8c288adfbfc0e4d75d72ad0679d672ebe5ce92
 };
--- routes/empruntRoutes.js
+++ routes/empruntRoutes.js
@@ -1,47 +1,8 @@
-<<<<<<< HEAD
 import express from "express";
 import { ajouterEmprunt } from "../controllers/empruntController.js";
 
 const router = express.Router();
 
 router.post("/emprunt", ajouterEmprunt);
-=======
-import express from 'express';
-
-const router = express.Router();
-
-// ajouter emprunt
-router.post('/', (req, res) => {
-    const { idClient, idLivre } = req.body;
-
-    res.json({
-        message: 'Emprunt ajouté',
-        data: { idClient, idLivre }
-    });
-});
-
-// get emprunts
-router.get('/:idClient', (req, res) => {
-    const idClient = req.params.idClient;
-
-    res.json({
-        message: 'Liste des emprunts',
-        data: [
-            { idLivre: 1 },
-            { idLivre: 2 }
-        ]
-    });
-});
-
-// retour livre
-router.post('/retour', (req, res) => {
-    const { idClient, idLivre } = req.body;
-
-    res.json({
-        message: 'Livre retourné',
-        data: { idClient, idLivre }
-    });
-});
->>>>>>> 4e8c288adfbfc0e4d75d72ad0679d672ebe5ce92
 
 export default router;
